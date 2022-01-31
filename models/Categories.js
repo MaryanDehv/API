aec8393da580ac3f789765090c70badeb00a9788
Maryan <maryanmodev@gmail.com>
2022-01-31 10:36:23 +0000

    udated to models/resolvers/

--- models/Categories.js
+++
@@ -1,9 +0,0 @@
-const {model , Schema} = require('mongoose');
-
-const categorySchema = new Schema({
-    name: String,
-    productCount: String
-});
-
-
-module.exports = model('Category' , categorySchema);
--- routes/dashboard.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get('/' , (req, res) => {
-    res.send('welcome to the dashboard')
-})
-
-
-module.exports = router;
